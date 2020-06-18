e1db35468ee72bc1741f71171c20e89603a2690b
kingyong <whistleyong@tencent.com>
2020-06-18 20:26:17 +0800

    feat: style

--- config/config.js
+++ config/config.js
@@ -1,37 +1,37 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  ssr: {},
+  ssr: {
+    devServerRender: false
+  },
   exportStatic: {},
   hash: true,
   mode: 'site',
   logo: '/logo.jpg',
   favicon: '/logo.jpg',
-  // exportStatic: { htmlSuffix: true },
-  // menus: {
-  //   '/principle': [
-  //     {
-  //       title: 'vue原理',
-  //       children: ['principle/index.md', 'principle/reactive_vue']
-  //     },
-  //     {
-  //       title: '状态管理',
-  //       children: ['principle/state_manager']
-  //     }
-  //   ],
-
-  //   '/summary': [
-  //     {
-  //       title: '总结',
-  //       children: [
-  //         'summary/extend_between_es6_and_es5',
-  //         'summary/unit_test_miniapp'
-  //       ]
-  //     }
-  //   ]
-  // },
-  // navs: [
-  //   null,
-  //   { title: 'GitHub', path: 'https://github.com/whistleyz' }
-  // ]
+  menus: {
+    '/summary': [
+      {
+        title: '总结',
+        children: [
+          'summary/extend_between_es6_and_es5',
+          'summary/unit_test_miniapp'
+        ]
+      }
+    ],
+    '/principle': [
+      {
+        title: 'vue原理',
+        children: ['principle/reactive_vue']
+      },
+      {
+        title: '状态管理',
+        children: ['principle/state_manager']
+      }
+    ]
+  },
+  navs: [
+    null,
+    { title: 'GitHub', path: 'https://github.com/whistleyz' }
+  ]
 })
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,11 @@
-Hello world.
+## 原理
+
+[深入了解 vue 响应式原理](/principle/reactive_vue)
+
+[实现一个前端状态管理器](/principle/state_manager)
+
+## 总结
+
+[ES6与ES5的类继承机制](/summary/extend_between_es6_and_es5)
+
+[小程序单元测试环境搭建](/summary/unit_test_miniapp)
