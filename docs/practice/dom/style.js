aacc0ad8da6a93af0ff7350d82e9ffe8a55f5b01
yongzhen <yongzhen@kuaishou.com>
2021-01-14 10:02:19 +0800

    feat: update docs

--- docs/practice/dom/style.js
+++ docs/practice/dom/style.js
@@ -3,9 +3,23 @@
  * @author: yongzhen
  * @Date: 2021-01-08 14:39:13
  * @LastEditors: yongzhen
- * @LastEditTime: 2021-01-08 14:39:51
+ * @LastEditTime: 2021-01-13 19:02:08
  */
 export const COMMENT_STYLE = `
+.comment-container {
+  background: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  z-index: 100000;
+  width: 400px;
+  height: 500px;
+  padding: 20px;
+  border: 1px solid #333;
+}
 .comment-container .tab-content {
   padding-top: 8px
 }
