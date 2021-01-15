da93d729ea68c87bd8ef9b7f6a7c74029d7c25fe
yongzhen <yongzhen@kuaishou.com>
2021-01-15 09:57:31 +0800

    feat: update docs

--- docs/practice/dom/comment.dist.js
+++ docs/practice/dom/comment.dist.js
@@ -520,11 +520,11 @@
    * @author: yongzhen
    * @Date: 2021-01-08 14:39:13
    * @LastEditors: yongzhen
-   * @LastEditTime: 2021-01-13 19:00:00
+   * @LastEditTime: 2021-01-14 10:06:54
    */
   const COMMENT_STYLE = `
 .comment-container {
-  backgroud-color: #fff;
+  background: #fff;
   position: fixed;
   top: 0;
   left: 0;
@@ -532,6 +532,10 @@
   bottom: 0;
   margin: auto;
   z-index: 100000;
+  width: 400px;
+  height: 600px;
+  padding: 20px;
+  border: 1px solid #333;
 }
 .comment-container .tab-content {
   padding-top: 8px
